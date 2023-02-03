30621aa96a12d01f309e828bb46036fe0731363d
AnarMikailov <a.v.mikayilov@gmail.com>
2023-02-03 17:56:20 +0400

    added navbar heading whatgpt3 components

--- responsive-fullsite/src/containers/whatGPT3/WhatGPT3.jsx
+++ responsive-fullsite/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./WhatGPT3.css";
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
